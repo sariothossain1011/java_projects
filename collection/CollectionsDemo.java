87fe364e67aacb8f3b7671243f2dd716ea2efb13
Sariot Hossain <sariothossain1011@gmail.com>
2025-05-30 00:01:14 +0600

    hashmap

--- collection/CollectionsDemo.java
+++ collection/CollectionsDemo.java
@@ -1,3 +1,12 @@
+package collection;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 public class CollectionsDemo {
 
